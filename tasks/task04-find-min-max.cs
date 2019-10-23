9e8d3a6db499205d1f81619a5686a53cedaf6126
ilya-figa <55840807+ilya-figa@users.noreply.github.com>
2019-10-23 19:27:51 +0300

    Update task04-find-min-max.cs

--- tasks/task04-find-min-max.cs
+++ tasks/task04-find-min-max.cs
@@ -8,101 +8,144 @@ using System;
 
 public class Program
 {
-	public static bool FindMinMax(int[][] array, out int min, out int max)
-	{
-		min = int.MinValue;
-		max = int.MaxValue;
+    public static bool FindMinMax(int[][] array, out int min, out int max)
+    {
+        min = int.MaxValue;
+        max = int.MinValue;
 
-		// ИЗМЕНИТЕ КОД ЭТОГО МЕТОДА
-		return false;
-	}
+        try
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException();
+            }
+            bool b = false;
 
-	// ДОБАВЬТЕ НОВЫЕ МЕТОДЫ, ЕСЛИ НЕОБХОДИМО
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    if (min > array[i][j])
+                    {
+                        min = array[i][j];
+                        b = true;
+                    }
+                    if (max < array[i][j])
+                    {
+                        max = array[i][j];
+                        b = true;
+                    }
+                }
+            }
 
-	// ----- ЗАПРЕЩЕНО ИЗМЕНЯТЬ КОД МЕТОДОВ, КОТОРЫЕ НАХОДЯТСЯ НИЖЕ -----
+            if (b == false)
+            {
+                min = 0;
+                max = 0;
+            }
+            Console.WriteLine(b + " " + min + " " + max);
+            return b;
+        }
+        catch (ArgumentNullException ex)
+        {
+            Console.WriteLine(ex);
+            throw;
+        }
+        catch
+        {
+            min = 0;
+            max = 0;
+            return false;
+        }
+    }
 
-	public static void Main()
-	{
-		Console.WriteLine("Task is done when all test cases are correct:");
-		
-		int testCaseNumber = 1;
+    // ДОБАВЬТЕ НОВЫЕ МЕТОДЫ, ЕСЛИ НЕОБХОДИМО
 
-		TestReturnedValues(testCaseNumber++, new int[][] { }, false, 0, 0);
-		TestReturnedValues(testCaseNumber++, new int[][] { null }, false, 0, 0);
-		TestReturnedValues(testCaseNumber++, new int[][] { new int[] { } }, false, 0, 0);
-		TestReturnedValues(testCaseNumber++, new int[][] { new int[] { 1 } }, true, 1, 1);
-		TestReturnedValues(testCaseNumber++, new int[][] { null, new int[] { } }, false, 0, 0);
-		TestReturnedValues(testCaseNumber++, new int[][] { new int[] { }, null }, false, 0, 0);
-		TestReturnedValues(testCaseNumber++, new int[][] { new int[] { 2 }, new int[] { 1 } }, true, 1, 2);
-		TestReturnedValues(testCaseNumber++, new int[][]
-			{
-				new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
-				new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 },
-				new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
-				new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }
-			} , true, 1, 14);
-		TestReturnedValues(testCaseNumber++, new int[][]
-			{
-				new int[] { 7, 395, 1, 9, 6, 62, 71, 80, 918 },
-				new int[] { 102, 72, 84, 41, 89, 382, 7, 36, 53, 10, 1101, 93, 23, 1401 },
-				new int[] { 942, 29, 3, 346, 53, 6, 73, 897, 384, 052, 3, 501 },
-				new int[] { 39, 83, 11, 28, 385, 0, 5, 482, 947, 143 }
-			} , true, 0, 1401);
-		TestException<ArgumentNullException>(testCaseNumber++, null);
+    // ----- ЗАПРЕЩЕНО ИЗМЕНЯТЬ КОД МЕТОДОВ, КОТОРЫЕ НАХОДЯТСЯ НИЖЕ -----
 
-		if (correctTestCaseAmount == testCaseNumber - 1)
-		{
-			Console.WriteLine("Task is done.");
-		}
-		else
-		{
-			Console.WriteLine("TASK IS NOT DONE.");
-		}
-	}
-	
-	private static void TestReturnedValues(int testCaseNumber, int[][] array, bool expectedResult, int expectedMin, int expectedMax)
-	{
-		try
-		{
-			int min, max;
-			var result = FindMinMax(array, out min, out max);
-			
-			if (result == expectedResult && expectedMin == min && expectedMax == max)
-			{
-				Console.WriteLine(correctCaseTemplate, testCaseNumber);
-				correctTestCaseAmount++;
-			}
-			else
-			{
-				Console.WriteLine(incorrectCaseTemplate, testCaseNumber);
-			}
-		}
-		catch(Exception)
-		{
-			Console.WriteLine(incorrectCaseTemplate, testCaseNumber);
-		}
-	}
+    public static void Main()
+    {
+        Console.WriteLine("Task is done when all test cases are correct:");
 
-	private static void TestException<T>(int testCaseNumber, int[][] array) where T : Exception
-	{
-		try
-		{
-			int min, max;
-			var result = FindMinMax(array, out min, out max);
-			Console.WriteLine(incorrectCaseTemplate, testCaseNumber);
-		}
-		catch (ArgumentException)
-		{
-			Console.WriteLine(correctCaseTemplate, testCaseNumber);
-			correctTestCaseAmount++;
-		}
-		catch (Exception)
-		{
-			Console.WriteLine(incorrectCaseTemplate, testCaseNumber);
-		}
-	}
+        int testCaseNumber = 1;
 
-	private static string correctCaseTemplate = "Case #{0} is correct.";
-	private static string incorrectCaseTemplate = "Case #{0} IS NOT CORRECT";
-	private static int correctTestCaseAmount = 0;
+        TestReturnedValues(testCaseNumber++, new int[][] { }, false, 0, 0);
+        TestReturnedValues(testCaseNumber++, new int[][] { null }, false, 0, 0);
+        TestReturnedValues(testCaseNumber++, new int[][] { new int[] { } }, false, 0, 0);
+        TestReturnedValues(testCaseNumber++, new int[][] { new int[] { 1 } }, true, 1, 1);
+        TestReturnedValues(testCaseNumber++, new int[][] { null, new int[] { } }, false, 0, 0);
+        TestReturnedValues(testCaseNumber++, new int[][] { new int[] { }, null }, false, 0, 0);
+        TestReturnedValues(testCaseNumber++, new int[][] { new int[] { 2 }, new int[] { 1 } }, true, 1, 2);
+        TestReturnedValues(testCaseNumber++, new int[][]
+            {
+                new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9 },
+                new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 },
+                new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 },
+                new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }
+            }, true, 1, 14);
+        TestReturnedValues(testCaseNumber++, new int[][]
+            {
+                new int[] { 7, 395, 1, 9, 6, 62, 71, 80, 918 },
+                new int[] { 102, 72, 84, 41, 89, 382, 7, 36, 53, 10, 1101, 93, 23, 1401 },
+                new int[] { 942, 29, 3, 346, 53, 6, 73, 897, 384, 052, 3, 501 },
+                new int[] { 39, 83, 11, 28, 385, 0, 5, 482, 947, 143 }
+            }, true, 0, 1401);
+        TestException<ArgumentNullException>(testCaseNumber++, null);
+
+        if (correctTestCaseAmount == testCaseNumber - 1)
+        {
+            Console.WriteLine("Task is done.");
+        }
+        else
+        {
+            Console.WriteLine("TASK IS NOT DONE.");
+        }
+        Console.ReadKey();
+    }
+
+    private static void TestReturnedValues(int testCaseNumber, int[][] array, bool expectedResult, int expectedMin, int expectedMax)
+    {
+        try
+        {
+            int min, max;
+            var result = FindMinMax(array, out min, out max);
+
+            if (result == expectedResult && expectedMin == min && expectedMax == max)
+            {
+                Console.WriteLine(correctCaseTemplate, testCaseNumber);
+                correctTestCaseAmount++;
+            }
+            else
+            {
+                Console.WriteLine(incorrectCaseTemplate, testCaseNumber);
+            }
+        }
+        catch (Exception)
+        {
+            Console.WriteLine(incorrectCaseTemplate, testCaseNumber);
+        }
+    }
+
+    private static void TestException<T>(int testCaseNumber, int[][] array) where T : Exception
+    {
+        try
+        {
+            int min, max;
+            var result = FindMinMax(array, out min, out max);
+            Console.WriteLine(incorrectCaseTemplate, testCaseNumber);
+        }
+        catch (ArgumentException)
+        {
+            Console.WriteLine(correctCaseTemplate, testCaseNumber);
+            correctTestCaseAmount++;
+        }
+        catch (Exception)
+        {
+            Console.WriteLine(incorrectCaseTemplate, testCaseNumber);
+        }
+    }
+
+    private static string correctCaseTemplate = "Case #{0} is correct.";
+    private static string incorrectCaseTemplate = "Case #{0} IS NOT CORRECT";
+    private static int correctTestCaseAmount = 0;
 }
